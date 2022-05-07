84027d22dc1e3cf31f07a07d4875c3f7659a0cb2
MD. Tanvir <mt14050@gmail.com>
2022-05-07 16:01:22 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+async function run() {
+    try {
+      
+  }
+    finally {
+        
+  }
+}
+
+run().catch(console.dir);
+
+//check
+app.get("/", (req, res) => {
+  res.send("assignment check");
+});
+
+app.listen(port);
