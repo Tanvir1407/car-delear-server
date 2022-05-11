b0dd099c5a0e1db5ff1933ce3dccee577c043824
MD. Tanvir <mt14050@gmail.com>
2022-05-11 14:17:36 +0600

    connect database

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+require("dotenv").config();
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -8,9 +11,30 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3ohgy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 async function run() {
+
     try {
-      app.get('/okk')
+      await client.connect();
+      const carsCollection = client.db("warehouse").collection("cars");
+      
+      //test
+      app.get('/cars', async (req, res) => {
+        const query = {}
+        const cursor = carsCollection.find(query);
+        const cars = await cursor.limit(6).toArray();
+        res.send(cars)
+      })
+
   }
     finally {
         
